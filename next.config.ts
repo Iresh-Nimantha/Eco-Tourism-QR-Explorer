a3100af92036484ac7e2756493fd911e5d35c630
Nipun Thilakshana Liyanage <thilakshana956@gmail.com>
2025-07-29 15:05:42 +0530

    push the feature of dynamic pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.shutterstock.com",
+      },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "dynamic-media-cdn.tripadvisor.com",
+      },
+    ],
+  },
+
+  devIndicators: {
+    buildActivity: false, // <-- This disables the "N" icon in dev mode
+  },
 };
 
 export default nextConfig;
